4f61955e26726f2a4876e74b47874c9d5f2e390a
evgen <parom.evgen@mail.ru>
2023-04-30 18:54:33 +0300

    prepare to get info from one nginx througth another nginx

--- k8s-web-to-nginx/index.mjs
+++ k8s-web-to-nginx/index.mjs
@@ -1,4 +1,5 @@
 import express from 'express'
+import fetch from 'node-fetch'
 import os from 'os'
 
 const app = express()
@@ -8,6 +9,13 @@ app.get("/", (req, res) => {
     const helloMessage = `VERSION 2: Hello from the ${os.hostname()}`
     console.log(helloMessage)
     res.send(helloMessage)
+})
+
+app.get("/nginx", async (req, res) => {
+    const url = 'http://nginx'
+    const response = await fetch(url);
+    const body = await response.text();
+    res.send(body)
 })
 
 app.listen(PORT, ()=> {
